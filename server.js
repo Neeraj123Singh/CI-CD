4dff92c8c91d37201d076bb95aab533772dd1995
Neeraj Singh <neerajroxx@Amans-MacBook-Air.local>
2022-10-02 20:42:20 +0530

    commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+function onRequest(request, response)
+{
+    if (request.url != '/') {
+        fs.readFile('./front/'+request.url, function(err, data) {
+            if (err) {
+                response.writeHead(404);
+                response.end();
+            } else {
+                response.end(data);
+            }
+        });
+    } else {
+        fs.readFile('front/index.html', function (err, data) {
+            if (err) {
+
+                response.writeHead(404);
+                response.end();
+            } else {
+                console.log("here")
+                response.writeHead(200,{'Content-Type': 'text/html'});
+                response.write(data);
+                response.end();
+            }
+
+        });
+    }
+}
+
+exports.onRequest = onRequest;
